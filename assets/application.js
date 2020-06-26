ff87a924e5d4e9a515e410b5ed94711560119134
Aaron <jerseypaint@gmail.com>
2020-06-26 01:29:25 -0400

    nearing v1.0

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,9 @@
 // Put your applicaiton javascript here
+function makeNavAppear() {
+    const primaryNav = document.getElementById("primaryNav");
+    const toggler = document.getElementById("toggler");
+
+    primaryNav.classList.toggle("show");
+    toggler.classList.toggle("close");
+    
+  };
